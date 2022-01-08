350bc5b9d712774a8cf416e85c35850aa1f372aa
shafayeat sumit <shafayeatkabir@gmail.com>
2022-01-08 16:36:13 +0600

    added .yml and chainlink

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,7 +1,22 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.6.6;
 
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
 contract Lottery {
-    function enter() public {}
+    address payable[] public players;
+    uint256 public usdEntryFee;
+    AggregatorV3Interface internal ethUsdPriceFeed;
+
+    constructor(address _priceFeedAddress) public {
+        usdEntryFee = 50 * (10**18);
+        ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+    }
+
+    function enter() public {
+        // $50 minimum
+        players.push(msg.sender);
+    }
 
     function getEntranceFee() public {}
 
