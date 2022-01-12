cab97a24a25cead8767f106b8b1b6c747e5a5e28
shafayeat sumit <shafayeatkabir@gmail.com>
2022-01-12 16:46:13 +0600

    test for getEntranceFee

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -50,7 +50,7 @@ def get_contract(contract_name):
 
 
 DECIMALS = 8 
-INITIAL_VALUE = 20000000000
+INITIAL_VALUE = 200000000000
 
 def deploy_mocks(decimals=DECIMALS, initial_value=INITIAL_VALUE):
     account = get_account()
--- tests/test_lottery.py
+++ tests/test_lottery.py
@@ -4,19 +4,39 @@
 # or in wei 15,9000000000,000000 WEI 
 
 from brownie import Lottery, accounts, config, network
+from scripts.deploy_lottery import deploy_lottery
 from web3 import Web3
+import pytest
+from scripts.helpful_scripts import (
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+    get_account,
+    fund_with_link,
+    get_contract,
+)
+
+
+# def test_get_entrance_fee():
+#     account = accounts[0]
+#     lottery = Lottery.deploy(config["networks"][network.show_active()]["eth_usd_price_feed"],{"from":account})    
+#     entrance_fee = lottery.getEntranceFee()
+#     print('entrance_fee',entrance_fee)
+#     assert lottery.getEntranceFee() > Web3.toWei(0.015, 'ether')
+    
+    
+
 
 def test_get_entrance_fee():
-    account = accounts[0]
-    lottery = Lottery.deploy(config["networks"][network.show_active()]["eth_usd_price_feed"],{"from":account})    
+    # we want to run all the unit test in local blockchain
+    # not in test net
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    # Arrange 
+    lottery = deploy_lottery()
+    # Act 
+    # 2000 eth/usd is 50    
+    # usdEntryFee is 50 
+    # 2000/1 == 50/x == 0.025
+    expected_entrance_fee = Web3.toWei(0.025,'ether')
     entrance_fee = lottery.getEntranceFee()
-    print('entrance_fee',entrance_fee)
-    assert lottery.getEntranceFee() > Web3.toWei(0.015, 'ether')
-    
-    
-    
-def test_only_owner():
-    account = accounts[0]    
-    lottery = Lottery.deploy(config["networks"][network.show_active()]["eth_usd_price_feed"],{"from":account})
-    lottery.endLottery()
-    
+    # Assert 
+    assert expected_entrance_fee == entrance_fee
